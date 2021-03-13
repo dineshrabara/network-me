5738304745b9c5233748e4603aec25afd4a11d7a
Dinesh Rabara <dinesh.rabara@gmail.com>
2021-03-13 12:30:57 +0530

    RND

--- background.js
+++ background.js
@@ -1,25 +1,6 @@
+
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ color: '#3aa757' }, function () {
-        console.log("The color is green.new");
+        console.log('The color is green.');
     });
-});
-chrome.devtools.network.onNavigated.addListener(function(){
-    alert("DevTools window opening.");
-});
-var openCount = 0;
-chrome.runtime.onConnect.addListener(function (port) {
-    alert("DevTools window opening.");
-    if (port.name == "devtools-page") {
-        if (openCount == 0) {
-            alert("DevTools window opening.");
-        }
-        openCount++;
-
-        port.onDisconnect.addListener(function (port) {
-            openCount--;
-            if (openCount == 0) {
-                alert("Last DevTools window closing.");
-            }
-        });
-    }
 });
